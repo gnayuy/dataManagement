88a7986c84254d5ba561368cb00c758f6a44b814
gnayuy <gnayuy@gmail.com>
2016-08-09 11:33:18 -0400

    add files

---
+++ src/datamanagement.cpp
@@ -0,0 +1,14 @@
+// datamanagement.cpp
+// manage mouse light data with dvid
+// developed by Yang Yu (gnayuy@gmail.com)
+
+#include "datamanagement.h"
+
+
+
+
+// main
+int main(int argc, char *argv[])
+{
+    return 0;
+}
---
+++ src/datamanagement.h
@@ -0,0 +1,14 @@
+// datamanagement.h
+// manage mouse light data with dvid
+// developed by Yang Yu (gnayuy@gmail.com)
+
+#ifndef __DATAMANAGEMENT_H__
+#define __DATAMANAGEMENT_H__
+
+#include "dataio.h"
+
+
+
+
+
+#endif // __DATAMANAGEMENT_H__
